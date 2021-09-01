6800a2de79cf857f4048b0b9a5733362210a6b9b
scott <scott.hsu@hgiga.com>
2021-09-01 08:35:40 +0800

    second commit

---
+++ ReturnJsonView/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReturnJsonView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TestViewToString()
+        {
+            string model = "我是model";
+            ViewData["TestViewData"] = "我是ViewData";
+            return Json(new
+            {
+                TestView = ConvertViewToString("_TestPartial", model, ViewData)
+            });
+        }
+
+        /// <summary>
+        /// 轉換View To String
+        /// </summary>
+        /// <param name="viewName">View名稱</param>
+        /// <param name="model">model</param>
+        /// <param name="viewData">viewData</param>
+        /// <returns></returns>
+        private string ConvertViewToString(string viewName, object model = null, ViewDataDictionary viewData = null)
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                if (viewData == null)
+                {
+                    ViewData.Model = model;
+                    viewData = ViewData;
+                }
+                else
+                {
+                    viewData.Model = model;
+                }
+
+                ViewEngineResult vResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+                ViewContext vContext = new ViewContext(this.ControllerContext, vResult.View, viewData, new TempDataDictionary(), writer);
+                vResult.View.Render(vContext, writer);
+                return writer.ToString();
+            }
+        }
+
+    }
+}
